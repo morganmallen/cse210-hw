0bcaf19b715b21c82adf0bb730aedb6597e48f38
Morgan Allen <morganallen1999@gmail.com>
2024-06-18 12:50:38 -0600

    edited prompts

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -5,11 +5,11 @@ public class PromptGenerator
 {
     private List<string> _prompts = new List<string>
     {
-        "Who was the most interesting person I interacted with today?",
-        "What was the best part of my day?",
-        "How did I see the hand of the Lord in my life today?",
-        "What was the strongest emotion I felt today?",
-        "If I had one thing I could do over today, what would it be?"
+        "What is something good that happened today?",
+        "What are you grateful for today?",
+        "Who did you talk to today?",
+        "How did you feel today?",
+        "Where did you go today?"
     };
 
     public string GetRandomPrompt()
